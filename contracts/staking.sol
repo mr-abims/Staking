45c6f8776a8cb23dd78f59b37efd1a3d59bf115b
mr-abims <adebayoabimbola9@gmail.com>
2022-03-21 16:48:00 +0100

    Updated scripts

--- contracts/staking.sol
+++ contracts/staking.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "./Iape.sol";
 contract Staking is ERC20 {
 
 
 uint rewardPerStake;
 address BoredApe = 0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D;
-IERC721 boredApe = IERC721(BoredApe);
+IAPE private ape = IAPE(BoredApe);
 struct stakeProfile{
     address staker;
     uint stakeStart;
@@ -20,10 +20,11 @@ mapping (address => stakeProfile)public Stakers;
         _mint(msg.sender, 200000 * 10 ** 18 );
     }
 
+
 function Stake(uint _amount) public  {
     stakeProfile storage Sp = Stakers[msg.sender];
     require(_balances[msg.sender] >=  _amount, "insufficient amount");
-    require(boredApe.balanceOf(msg.sender) >= 1, "Not a boredApe owner");
+    require(ape.balanceOf(msg.sender) >= 1, "Not a boredApe owner");
     if(Sp.status = true){
         uint256 daySpent = block.timestamp - Sp.stakeStart;
         if(daySpent > 3 days){
@@ -47,14 +48,15 @@ Sp.stakeStart = block.timestamp;
 
 
 
-function withdrawSTake(uint256 _amountToWithdraw) public returns(uint256) {
+function withdrawSTake(uint256 _amountToWithdraw) public returns(uint256 total) {
     stakeProfile storage Sp = Stakers[msg.sender];
     require(Sp.status == true, "You are not allowed to use this function");
    
    uint256 daySpent = block.timestamp - Sp.stakeStart;
-   if(daySpent > 3 days){
+   if(daySpent > 3 seconds){
        uint256 interests = Sp.amountStaked * (daySpent/ 86400) / 300;
-       Sp.amountStaked += interests;   
+       Sp.amountStaked += interests;
+       Sp.amountStaked = total;   
    }
    require(Sp.amountStaked >_amountToWithdraw, "Insufficient funds");
 
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,16 +1,48 @@
 import { ethers } from "hardhat";
+import { Signer } from "ethers";
 
 async function main() {
+  const owner = "0x6315AE672539D1d843F0581a11102a77A948f671";
+  const ownerSigner = await ethers.getSigner(owner);
+  // @ts-ignore
+  await hre.network.provider.request({
+    method: "hardhat_impersonateAccount",
+    params: [owner],
+  });
+  // @ts-ignore
+  await hre.network.provider.send("hardhat_setBalance", [
+    owner,
+    "0x2000000000000000000000000000000",
+  ]);
+
   const Staking = await ethers.getContractFactory("Staking");
-  const staking = await Staking.deploy("Boredape", "BRT");
+  const staking = await Staking.connect(ownerSigner).deploy("Boredape", "BRT");
   await staking.deployed();
-  console.log("staking address", staking.address);
+  const balance = await ethers.getContractAt("IERC20", staking.address);
+  const bal = await balance.balanceOf(owner);
+  console.log(bal);
   const border = "0x2C804244D7CA969fA75c866891dAF6F3f1f8B174";
 
-  const sign = await ethers.getSigner(border);
-  const Stake = await staking.Stake(20);
+  await balance.connect(ownerSigner).transfer(border, 20);
+  const balanceOfBorder = await balance.balanceOf(border);
+  console.log(balanceOfBorder);
+
+  console.log("staking address", staking.address);
+  // @ts-ignore
+  await hre.network.provider.request({
+    method: "hardhat_impersonateAccount",
+    params: [border],
+  });
+  // @ts-ignore
+  await hre.network.provider.send("hardhat_setBalance", [
+    border,
+    "0x2000000000000000000000000000000",
+  ]);
+
+  const sign: Signer = await ethers.getSigner(border);
+  const Stake = await staking.connect(sign).Stake(10);
   console.log(Stake);
-  const withdraw = await staking.connect(sign).withdrawSTake(10);
+  const withdraw = await staking.connect(sign).withdrawSTake(2);
   console.log(withdraw);
 }
 
